9ca16fc56d84874695bafdb33ace4c08b9997a30
winner-hue <1344246287@qq.com>
2022-04-05 16:01:27 +0800

    init

---
+++ src/main/java/icu/fanjie/Requests.java
@@ -0,0 +1,11 @@
+package icu.fanjie;
+
+public class Requests {
+    public static Response get() {
+        return new Response();
+    }
+
+    public static Response post() {
+        return new Response();
+    }
+}
